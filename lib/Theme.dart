61b39bb727fdb0d1c68d27d996e58a94b1538dfe
Keval Prajapati <keval.prajapatimus@gmail.com>
2021-05-10 14:20:24 +0530

    Login screen

---
+++ lib/Theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  static const themeDark = Color(0xFF101010);
+  static const themeRed = Color(0xFFF40308);
+  static const themeGrey = Color(0xFF1D1D1D);
+  static const themeLightGrey = Color(0xFF464646);
+}
