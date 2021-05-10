85db4f08fa9cf6780406fa2be2b4d15da4e03bbc
Siddhant Mehta <2010015323@adit.ac.in>
2021-05-10 14:01:00 +0530

    homepage desinged

--- lib/Screens/HomePage.dart
+++ lib/Screens/HomePage.dart
@@ -1,8 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:shape_of_view/shape_of_view.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //double height = MediaQuery.of(context).size.height;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        backgroundColor: Colors.pink,
+        body: (ShapeOfView(
+          elevation: 10,
+          height: 375,
+          shape: DiagonalShape(
+              position: DiagonalPosition.Bottom,
+              direction: DiagonalDirection.Right,
+              angle: DiagonalAngle.deg(angle: 15)),
+          child: Container(
+            color: Colors.lightBlue,
+            child: Center(
+              child: Text(
+                'Notes',
+                style: TextStyle(color: Colors.black, fontSize: 30),
+              ),
+            ),
+          ),
+        )));
   }
 }
